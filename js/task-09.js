81fe985eb9904eb6390b729266e09babd288fc3c
Anton <anton_surkov@rambler.ru>
2022-06-14 19:00:56 +0300

    comlete task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  BODY: document.body,
+  CHANGE_COLOR_BTN: document.querySelector(".change-color"),
+  OUTPUT_COLOR: document.querySelector(".color"),
+};
+
+refs.CHANGE_COLOR_BTN.addEventListener("click", onChengeColorBtn);
+
+function onChengeColorBtn() {
+  refs.BODY.style.backgroundColor = getRandomHexColor();
+  refs.OUTPUT_COLOR.textContent = getRandomHexColor();
+}
