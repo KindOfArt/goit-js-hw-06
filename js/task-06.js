fb207602f91805b42ff206904af81cb82288ad76
Anton <anton_surkov@rambler.ru>
2022-06-14 11:50:26 +0300

    fix hw-6

--- js/task-06.js
+++ js/task-06.js
@@ -2,8 +2,10 @@ const inputRef = document.querySelector("#validation-input");
 const inputDataLength = +inputRef.getAttribute("data-length");
 
 inputRef.addEventListener("blur", () => {
-  console.log(inputRef.value.length);
-  if (inputRef.value.length < inputDataLength) {
+  if (
+    inputRef.value.length < inputDataLength ||
+    inputRef.value.length > inputDataLength
+  ) {
     inputRef.classList.remove("valid");
     return inputRef.classList.add("invalid");
   }
